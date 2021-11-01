ffb7b4e330ae88396acdb4fd360b746eb175e9df
Yaara Shabtai <yaara10@live.com>
2021-11-01 18:47:17 +0200

    app.run

--- app.py
+++ app.py
@@ -46,8 +46,11 @@ def cart():
 	return render_template('cart.html', items=query_all(), s_item_idx=s_item_idx, num_items=num_items) 
 
 ## And doesn't go after this line.  ##
-if __name__ == "__main__":  # Makes sure this is the main process
-	app.run(# Starts the site
-		host='0.0.0.0',     # EStablishes the host, required for repl to detect the site
-		port=8080,          # Randomly select the port the machine hosts on.
-        debug=True)
+#if __name__ == "__main__":  # Makes sure this is the main process
+#	app.run(# Starts the site
+#		host='0.0.0.0',     # EStablishes the host, required for repl to detect the site
+#		port=8080,          # Randomly select the port the machine hosts on.
+#        debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
